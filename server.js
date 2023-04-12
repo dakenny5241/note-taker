91e450655e62a9d8699c400f0ea255ff62d42287
dakenny5241 <123331009+dakenny5241@users.noreply.github.com>
2023-04-12 10:45:04 +0100

    post on /api/notes done

--- server.js
+++ server.js
@@ -14,19 +14,31 @@ app.use(express.static('public'));
 
 // GET Route for homepage
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 // GET Route for feedback page
 app.get('/api/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/db/db.json'))
+    res.sendFile(path.join(__dirname, '/db/db.json'))
 );
 
-// Wildcard route to direct users to a 404 page
+// Wildcard route to direct users to a index.html page
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/index.html'))
+    res.sendFile(path.join(__dirname, 'public/index.html'))
 );
+
+app.post("/api/notes", function (req, res) {
+    let existingNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let newNote = req.body;
+    let newID = existingNotes.length.toString();
+    newNote.id = newID;
+    existingNotes.push(newNote);
+    fs.writeFileSync("./db/db.json", JSON.stringify(existingNotes));
+    res.json(existingNotes);
+  
+}
+  
 
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
